bb48ed193d22bbc28ccadef60fe85743149daf0b
Jinge Zhang <jinge_windows@hotmail.com>
2023-10-14 04:46:21 +1100

    开始测试静态文件的加载速度

--- BlazorApp_AzureStaticWeb_Test/Client/Program.cs
+++ BlazorApp_AzureStaticWeb_Test/Client/Program.cs
@@ -14,6 +14,8 @@ namespace BlazorApp_AzureStaticWeb_Test
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+
+
             await builder.Build().RunAsync();
         }
     }
