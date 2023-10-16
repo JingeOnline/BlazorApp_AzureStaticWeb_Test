cdde4faf14be01f4c62d2b5f71f2c1e6a03a28c7
Jinge Zhang <jinge_windows@hotmail.com>
2023-10-16 22:01:27 +1100

    更新了js代码的位置

--- BlazorApp_AzureStaticWeb_Test/wwwroot/js/site.js
+++ BlazorApp_AzureStaticWeb_Test/wwwroot/js/site.js
@@ -1,1 +1,6 @@
 ﻿
+function activateCarousel() {
+    //手动激活幻灯片的第一张图片，否则手机端无法滑动，且自动放映不起作用。
+    var myCarousel = document.querySelector('#carouselExampleIndicators');
+    var carousel = new bootstrap.Carousel(myCarousel);
+}
